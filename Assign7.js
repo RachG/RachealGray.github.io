b49e1ccf37805b90e78f03fca82d4308ac503233
RachealGray <rachealgray25@gmail.com>
2017-04-04 22:32:13 -0500

    Add files via upload

--- Assign7.js
+++ Assign7.js
@@ -1,4 +1,43 @@
 //JavaScript Day and Time 
+function validateForm() {
+    var x = document.forms["myForm"]["firstname"].value;
+    if (x == "") {
+        alert("First name must be filled out");
+        return false;
+    }
+}
+
+function validateForm() {
+    var y = document.forms["myForm"]["lastname"].value;
+    if (y == "") {
+        alert("Last name must be filled out");
+        return false;
+    }
+}
+
+function validateForm() {
+    var z = document.forms["myForm"]["email"].value;
+    if (z == "") {
+        alert("Email must be filled out");
+        return false;
+    }
+}
+
+function validateForm() {
+    var s = document.forms["myForm"]["Question", "Problem", "Order Status Update"].value;
+    if (s == "") {
+        alert("Please select Options");
+        return false;
+    }
+}
+
+function validateForm() {
+    var c = document.forms["myForm"]["message"].value;
+    if (c == "") {
+        alert("Please fill out the message box");
+        return false;
+    }
+}
 
 function startTime() {
     var today = new Date();
@@ -11,7 +50,7 @@ function startTime() {
     minutes = checkTime(minutes);
     second = checkTime(second);
 
-    document.getElementById('dateTime').innerHTML =
+    document.getElementById("dateTime").innerHTML =
         "Today is " + month + "/" + day + "/" + year +
         " and the time is " + hours + ":" + minutes + ":" + second;
     var t = setTimeout(startTime, 500);
